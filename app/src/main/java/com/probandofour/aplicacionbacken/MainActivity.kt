e7364ac961650b2ee7ef452a69dcb9a9d91d29cb
Estalin1412 <jose.munoz.d@uni.pe>
2024-08-19 20:51:02 -0500

    Agregando Ruta AplicacionBacken

---
+++ app/src/main/java/com/probandofour/aplicacionbacken/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.probandofour.aplicacionbacken
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    public lateinit var boton: Button
+    public lateinit var vText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        vText = findViewById(R.id.IdUsuario)
+        boton = findViewById(R.id.button)
+
+        boton.setOnClickListener{
+            val variableTexto =vText.text.toString()
+            Toast.makeText(this, "Bienvenido ${variableTexto}!!", Toast.LENGTH_SHORT).show()
+            if(variableTexto.isNotEmpty()){
+                
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/probandofour/aplicacionbacken/ResultActivity2.kt
@@ -0,0 +1,28 @@
+package com.probandofour.aplicacionbacken
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultActivity2 : AppCompatActivity() {
+    public lateinit var texVePan2: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        texVePan2 = findViewById(R.id.TextoPan2)
+
+
+
+    }
+}
